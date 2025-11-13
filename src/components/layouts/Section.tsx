5f8127bedb8fd23104950ba31bedaba47605cd1a
fahkhaer <khaeranifah@gmail.com>
2025-11-14 05:50:14 +0700

    Section class name

--- src/components/layouts/Section.tsx
+++ src/components/layouts/Section.tsx
@@ -1,22 +1,29 @@
+import { cn } from '@/src/lib/utils';
+
 type SectionProps = {
   children: React.ReactNode;
   title: string;
   subtitle: string;
   id: string;
+  className?: string;
 };
 
-const Section: React.FC<SectionProps> = ({ children, title, subtitle, id }) => {
+const Section: React.FC<SectionProps> = ({
+  children,
+  title,
+  subtitle,
+  id,
+  className,
+}) => {
   return (
     // heading
-    <div className='custom-container' id={id}>
+    <div className={cn('custom-container', className)} id={id}>
       <div className='text-center'>
         <h2 className='display-xl-bold text-neutral-950'>{title}</h2>
         <p className='text-md-medium text-neutral-950'>{subtitle}</p>
       </div>
       {/* content */}
-      <div className="mt-6 md:mt-16">
-        {children}  
-      </div>
+      <div className='mt-6 md:mt-16'>{children}</div>
     </div>
   );
 };
