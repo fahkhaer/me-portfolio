b68052fbc1576060b34a0816cbd7d5ca14dd12d3
fahkhaer <khaeranifah@gmail.com>
2025-11-13 07:23:54 +0700

    layouting hero elements

--- src/app/home/partials/hero.tsx
+++ src/app/home/partials/hero.tsx
@@ -1,44 +1,48 @@
 import { Button } from '@/src/components/ui/button';
-import { link } from 'fs';
-import { ArrowRightCircle, Link, Mic } from 'lucide-react';
 import Image from 'next/image';
 import { Icon } from '@iconify/react';
-import TechLogo from '@/src/components/ui/tech-logo';
 import Statistics from '@/src/components/ui/statistic';
 
 const Hero = () => {
   return (
-    <div className='relative h-screen'>
+    <div className='relative pl-30 md:h-225'>
       {/* kanan */}
-      <Button asChild className='bottom-0' variant={'default'}>
-        {/* <TechLogo  logo='public/icons/js.png' /> */}
-        {/* <Link href=''> Contact Me </Link> */}
-      </Button>
+      {/* <Button asChild className='bottom-0' variant={'default'}>
+        <TechLogo  logo='public/icons/js.png' />
+        <Link href='#'> Contact Me </Link>
+      </Button> */}
       <div className='absolute right-0'>
         <Statistics />
-        <Button className='bg-secondary-100'>Contact Me 
-          <ArrowRightCircle className='bg-neutral-950 rounded-full ' />
+        <Button className='bg-secondary-100 flex h-14 w-full justify-between rounded-full py-2 pr-2 pl-4 hover:bg-[#D9A23F]'>
+          <p className='text-neutral-950'>Contact Me </p>
+
+          <Icon
+            icon='lucide:arrow-right'
+            width='36'
+            height='36'
+            className='size-9 rounded-full bg-neutral-950 p-2'
+          />
         </Button>
       </div>
 
       {/* tengah */}
       <div
         style={{ height: 'clamp(23.44rem, 61.25vw, 45.94rem)' }}
-        className='bottom-0'
+        className=''
       >
-        {' '}
         <Image
           src='/images/hero.png'
           alt='hero-image'
           width={610}
           height={735}
+          className=''
         />
       </div>
 
       {/* kiri */}
       <div></div>
       {/* hi i'm edwin */}
-      <div className='left-1 flex w-113 flex-col gap-3.5 text-white'>
+      <div className='absolute bottom-[144] left-0 flex w-113 flex-col gap-3.5 text-white'>
         <div>
           <Icon
             icon='fluent:mic-24-filled'
