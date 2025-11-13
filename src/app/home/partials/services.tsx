4ac0e9134520651c0df335eaf568900f818f1a3b
fahkhaer <khaeranifah@gmail.com>
2025-11-14 05:50:00 +0700

    sevice page

--- src/app/home/partials/services.tsx
+++ src/app/home/partials/services.tsx
@@ -1,14 +1,27 @@
 import Section from '@/src/components/layouts/Section';
+import TechLogo from '@/src/components/ui/tech-logo';
+import { servicesData } from '@/src/constants/services-data';
+import Image from 'next/image';
 import React from 'react';
 
-function services() {
-  return <></>;
+function Services() {
+  return (
+    <>
+      <div className='divide-primary-300 divide-y flex items-start gap-5'>
+        {servicesData.map((service, index) => (
+          <div key={index}>
+            <div className='size-16 rounded-full border border-neutral-300'>
+              {service.logo}
+            </div>
+            <p className='display-xs-bold'>{service.title}</p>
+            <p className='text-md-regular'>{service.subtitle}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default services;
+export default Services;
 
-type ServiceCardsProps = {
-  children: React.ReactNode;
-};
 
-const ServiceCard = () => {};
---
+++ src/constants/services-data.ts
@@ -0,0 +1,27 @@
+
+type Service = {
+  logo: React.ReactNode;
+  title: string;
+  subtitle: string;
+};
+
+export const servicesData: Service[] = [
+  {
+    logo: '⚙️',
+    title: 'COMPONENT-BASED DEVELOPMENT',
+    subtitle:
+      'Reusable, scalable code built with modern frameworks like React or Vue.',
+  },
+  {
+    logo: '🎨',
+    title: 'PIXEL-PERFECT UI IMPLEMENTATION',
+    subtitle:
+      'Translating design into high-fidelity user interfaces with attention to detail.',
+  },
+  {
+    logo: '📱',
+    title: 'RESPONSIVE & ACCESSIBLE DESIGN',
+    subtitle:
+      'Optimized layouts that work seamlessly across all screen sizes and devices.',
+  },
+];
