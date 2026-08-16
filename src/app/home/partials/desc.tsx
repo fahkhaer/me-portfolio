54dc840d5880769c37a38e5f61f2bb361e35d1c3
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-08-16 10:01:33 +0700

    Refactor Desc component to display static text instead of animated typing effect

--- src/app/home/partials/desc.tsx
+++ src/app/home/partials/desc.tsx
@@ -1,26 +1,9 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-
 const Desc = () => {
-  const fullText =
-    'From Figma designs to fullstack applications, I build responsive, interactive, and user-focused digital experiences.';
-  const [displayText, setDisplayText] = useState('');
-
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      setDisplayText((prev) => prev + fullText[index]);
-      index++;
-      if (index >= fullText.length) clearInterval(interval);
-    }, 20);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div id='about' className='py-20 text-center'>
       <p className='custom-container display-lg-medium -tracking-tight'>
-        {displayText}
-        <span className='animate-pulse'>|</span>
+        From Figma designs to fullstack applications, I build responsive,
+        interactive, and user-focused digital experiences.
       </p>
     </div>
   );
